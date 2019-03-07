2f4395527b5e95e88c7ae010b30f5a4cff809c4f
Duncan Thomas <duncan.thomas@gmail.com>
2019-03-07 19:56:24 +0000

    More tutorial steps
    https://github.com/grommet/grommet-starter-new-app
    
    We've now done 'Adding Box', 'Improving the AppBar', 'Adding a main
    body' and 'Adding State'

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
-import { Grommet } from 'grommet';
+import { Box, Button, Heading, Grommet } from 'grommet';
+import { Notification } from 'grommet-icons';
 
 const theme = {
   global: {
+    colors: {
+      brand: '#228BE6',
+    },
     font: {
       family: 'Roboto',
       size: '14px',
@@ -11,23 +15,52 @@ const theme = {
   },
 };
 
+const AppBar = (props) => (
+  <Box
+    tag='header'
+    direction='row'
+    align='center'
+    justify='between'
+    background='brand'
+    pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+    elevation='medium'
+    style={{ zIndex: '1' }}
+    {...props}
+  />
+);
+
 class App extends Component {
+  state = {
+    showSidebar: false,
+  }
+
   render() {
+    const { showSidebar } = this.state;
     return (
-      <Grommet theme={theme}>
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <Grommet theme={theme} full>
+        <AppBar>
+          <Heading level='3' margin='none'>My App</Heading>
+          <Button
+            icon={<Notification />}
+            onClick={() => this.setState(prevState => ({ showSidebar: !prevState.showSidebar }))}
+          />
+        </AppBar>
+        <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
+          <Box flex align='center' justify='center'>
+            app body
+          </Box>
+          {showSidebar && (
+            <Box
+              width='medium'
+              background='light-2'
+              elevation='small'
+              align='center'
+              justify='center'
+            >
+              sidebar
+            </Box>
+          )}
+        </Box>
       </Grommet>
     );
   }
